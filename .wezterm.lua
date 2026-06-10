a55a7cfdfa286467eaf7f78f3819d9b178f71fee
Loïc Villet <villet.loic@gmail.com>
2026-06-10 04:23:10 +0200

    (wezterm): allow shorter neovim title to persist

--- .wezterm.lua
+++ .wezterm.lua
@@ -176,7 +176,8 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, hover, max_width)
   local parent_folder, cwd = get_last_two_path_segments(tab)
 
   -- Check if the tab has a custom title set by Neovim set through OSC sequences (should have "[nvim]" in the title)
-  if tab.active_pane.title and tab.active_pane.title:match("eovim") then
+  local pane_title = tab.active_pane.title or ""
+  if pane_title and (pane_title:match("eovim") or pane_title:match("")) then
     local title = string.format("%s%d: %s", zoom_icon, tab_number, tab.active_pane.title)
     return {
       { Text = " " .. title .. " " },
