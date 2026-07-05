41f316ede630e5e354a345676e9857e796e9996a
Loïc Villet <villet.loic@gmail.com>
2026-07-05 20:44:55 +0200

    (tampermonkey): add author to script lul

--- browser/tampermonkey/prevent-page-load-stop-esc.js
+++ browser/tampermonkey/prevent-page-load-stop-esc.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Prevents ESC key from stopping page loads but allows Ctrl+Q to send ESC
-// @author       You
+// @author       Loïc Villet
 // @match        *://*/*
 // @run-at       document-start
 // @grant        none
